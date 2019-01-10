a6742d2687c668aa2e513b2c734b9a6938d226dd
Saiba Kataruka <saibakataruka@Saibas-MacBook-Pro.local>
2019-01-10 15:04:32 +0800

    Space Shooter v1

---
+++ DestroyByContact.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour
+{
+
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, transform.position, transform.rotation);
+        }
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Mover.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb; 
+
+     void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0.0f, 0.0f, speed);
+    }
+}
